594fc9d9ac6a6eb8f788b55c2805b46d87c822e7
mkulagin <1844280+mkulagin@users.noreply.github.com>
2025-10-25 15:56:47 +0300

    mvp version v0.40.0

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -25,6 +25,8 @@ ExternalProject_Add(mpv
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_REMOTE_NAME origin
+    GIT_TAG v0.40.0
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
